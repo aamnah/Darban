388e7f0a8ce6af2e3295aa663313ee6cd7052f2d
Aamnah <hello@aamnah.com>
2016-09-21 15:14:19 +0500

    + comments

--- nodemcu/config.lua
+++ nodemcu/config.lua
@@ -2,16 +2,19 @@
 
 local module = {}
 
+-- WiFi
 module.SSID=""
 module.PASSWORD=""
+
+-- MQTT
 module.BROKER1="192.168.0.112"
 module.BROKER2="192.168.0.200"
-module.ENDPOINT="smartlock/"
 module.PORT=1883
 module.ID="SmartLock: " .. wifi.sta.getmac()
-module.KEEPALIVE=18000
-module.QOS=2 --exactly once
 module.USER=""
 module.PASS=""
+module.KEEPALIVE=18000
+module.QOS=2 --exactly once
+module.ENDPOINT="smartlock/"
 
 return module
