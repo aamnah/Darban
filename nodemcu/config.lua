5901f77ce6ac4b61035368a4ed0f1442730ac876
Aamnah <hello@aamnah.com>
2016-09-21 14:41:25 +0500

    +

---
+++ nodemcu/config.lua
@@ -0,0 +1,16 @@
+-- config.lua
+
+local module = {}
+
+module.SSID=""
+module.PASSWORD=""
+module.BROKER1="192.168.0.112"
+module.BROKER2="192.168.0.200"
+module.ENDPOINT="smartlock/"
+module.PORT=1883
+module.ID="SmartLock: " .. wifi.sta.getmac()
+module.KEEPALIVE=18000
+module.USER=""
+module.PASS=""
+
+return module
