d0a23f140d82f0bdd5a845ef83c866a5d6e4019a
Aamnah <hello@aamnah.com>
2016-09-22 12:43:27 +0500

    reduced keepalive

--- nodemcu/config.lua
+++ nodemcu/config.lua
@@ -12,12 +12,13 @@ module.PASSWORD=""
 -- MQTT
 module.BROKER1="192.168.0.112"
 module.BROKER2="192.168.0.200"
+module.BROKER3="10.0.0.101"
 module.PORT=1883
 module.ID="SmartLock: " .. wifi.sta.getmac()
 module.USER=""
 module.PASS=""
-module.KEEPALIVE=18000
+module.KEEPALIVE=120
 module.QOS=2 --exactly once
-module.ENDPOINT="smartlock/"
+module.ENDPOINT="home/smartlock"
 
 return module
